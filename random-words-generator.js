40190367d2365a8e99ba31098fd045e59adb9dd4
Vladimir Lehmann <vladimirlehmann1@gmail.com>
2023-05-27 20:53:46 +0200

    Init project

---
+++ random-words-generator.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+exports.RandomWordsGenerator = void 0;
+var RandomWordsGenerator = /** @class */ (function () {
+    function RandomWordsGenerator() {
+    }
+    RandomWordsGenerator.prototype.generate = function (words) {
+        return "random_word";
+    };
+    return RandomWordsGenerator;
+}());
+exports.RandomWordsGenerator = RandomWordsGenerator;
---
+++ random-words-generator.ts
@@ -0,0 +1,7 @@
+export class RandomWordsGenerator {
+
+    public generate(words: string[]): string {
+        return "random_word";
+    }
+
+}
