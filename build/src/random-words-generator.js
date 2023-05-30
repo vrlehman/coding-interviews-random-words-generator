7dcd1a22b6b13da85a25541a69f6c06e9f3afa09
Vladimir Lehmann <vladimirlehmann1@gmail.com>
2023-05-31 01:46:24 +0200

    removed node_modules and build folder

--- build/src/random-words-generator.js
+++
@@ -1,7 +0,0 @@
-export class RandomWordsGenerator {
-    generate(words) {
-        if (words === null || words === undefined || words.length === 0)
-            return undefined;
-        return "random_word2";
-    }
-}
--- build/test/random-words-generator.spec.js
+++
@@ -1,8 +0,0 @@
-import { expect } from "chai";
-import { RandomWordsGenerator } from "../src/random-words-generator";
-describe('RandomWordsGenerator Tests', () => {
-    it('generate with empty words delivers undefined', async () => {
-        const generator = new RandomWordsGenerator();
-        expect(generator.generate(['a'])).to.not.exist;
-    });
-});
