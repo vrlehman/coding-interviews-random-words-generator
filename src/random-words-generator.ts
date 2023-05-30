168d352d65df280f8d4c1719ae4af0af2bd5b98c
Vladimir Lehmann <vladimirlehmann1@gmail.com>
2023-05-31 01:43:51 +0200

    Updated mocha config, generate first and last letters selection

--- src/random-words-generator.ts
+++ src/random-words-generator.ts
@@ -1,8 +1,17 @@
 export class RandomWordsGenerator {
-
+    
     public generate(words: string[]): string | undefined {
-        if(words === null || words === undefined || words.length === 0) return undefined;
-        return "random_word2";
+        if (words === null || words === undefined || words.length === 0) return undefined;
+
+        const firtsLettersArray: string[] = this.generateFirstLettersSelection(words);
+
+        return 'random_word1';
     }
 
+    public generateFirstLettersSelection(words: string[]): string[] {
+        return [...new Set(words.map((word: string) => word.charAt(0)))];
+    }
+    public generateLastLettersSelection(words: string[]): string[] {
+        return [...new Set(words.map((word: string) => word.charAt(word.length - 1)))];
+    }
 }
--- test/random-words-generator.spec.ts
+++ test/random-words-generator.spec.ts
@@ -2,8 +2,20 @@ import { expect } from "chai";
 import { RandomWordsGenerator } from "../src/random-words-generator";
 
 describe('RandomWordsGenerator Tests', () => {
+    it('generate first letters works correctly', async () => {
+        const generator = new RandomWordsGenerator();
+        const words = ['banana', 'apple', 'cabbage', 'cucumber'];
+        expect(generator.generateFirstLettersSelection(words)).to.eql(['b', 'a', 'c']);
+    });
+
+    it('generate last letters works correctly', async () => {
+        const generator = new RandomWordsGenerator();
+        const words = ['banana', 'apple', 'cabbage', 'cucumber'];
+        expect(generator.generateLastLettersSelection(words)).to.eql(['a', 'e', 'r']);
+    });
+
     it('generate with empty words delivers undefined', async () => {
         const generator = new RandomWordsGenerator();
-        expect(generator.generate(['a'])).to.not.exist;
+        expect(generator.generate([])).to.not.exist;
     });
 })
