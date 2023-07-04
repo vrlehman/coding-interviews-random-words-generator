23a098960faf822459d4cac4f1bce6b10c61aaaa
Vladimir Lehmann <vladimirlehmann1@gmail.com>
2023-07-04 15:52:39 +0200

    changed module resolution

--- src/random-words-generator.ts
+++ src/random-words-generator.ts
@@ -1,9 +1,13 @@
 export class RandomWordsGenerator {
-    
+
     public generate(words: string[]): string | undefined {
         if (words === null || words === undefined || words.length === 0) return undefined;
 
-        const firtsLettersArray: string[] = this.generateFirstLettersSelection(words);
+        const firtsLettersSelection: string[] = this.generateFirstLettersSelection(words);
+        const possibleLetterPairs = this.generatePossibleLetterPairs(words);
+        const lastLettersSelection: string[] = this.generateLastLettersSelection(words);
+
+        const randomFirstLetter = firtsLettersSelection[Math.floor(Math.random() * firtsLettersSelection.length)];
 
         return 'random_word1';
     }
@@ -13,5 +17,28 @@ export class RandomWordsGenerator {
     }
     public generateLastLettersSelection(words: string[]): string[] {
         return [...new Set(words.map((word: string) => word.charAt(word.length - 1)))];
+    }
+
+    public generatePossibleLetterPairs(words: string[]) {
+
+        let letterPairs = new Map<string, Set<string>>();
+
+        words.forEach((word: string) => {
+            for(let i = 0; i <= word.length - 2; i++) {
+                if(letterPairs.has(word[i])) {
+                    let values = letterPairs.get(word[i]) ?? new Set<string>();
+                    values.add(word[i+1]);
+                } else {
+                    let values = new Set<string>(word[i+1]);
+                    letterPairs.set(word[i], values);
+                }
+            }
+
+
+        })
+
+
+
+        return letterPairs;
     }
 }
--- test/random-words-generator.spec.ts
+++ test/random-words-generator.spec.ts
@@ -14,6 +14,20 @@ describe('RandomWordsGenerator Tests', () => {
         expect(generator.generateLastLettersSelection(words)).to.eql(['a', 'e', 'r']);
     });
 
+    it('generate letter pairs works correctly', async () => {
+        const generator = new RandomWordsGenerator();
+        const words = ['banana', 'apple', 'cabbage'];
+        const letterPairs = generator.generatePossibleLetterPairs(words);
+        expect(letterPairs.size).to.equal(7);
+        expect(letterPairs.get('b')).to.eql(new Set(['a', 'b']));
+        expect(letterPairs.get('a')).to.eql(new Set(['n', 'p', 'b', 'g']));
+        expect(letterPairs.get('n')).to.eql(new Set(['a']));
+        expect(letterPairs.get('p')).to.eql(new Set(['p', 'l']));
+        expect(letterPairs.get('l')).to.eql(new Set(['e']));
+        expect(letterPairs.get('c')).to.eql(new Set(['a']));
+        expect(letterPairs.get('g')).to.eql(new Set(['e']));
+    });
+
     it('generate with empty words delivers undefined', async () => {
         const generator = new RandomWordsGenerator();
         expect(generator.generate([])).to.not.exist;
